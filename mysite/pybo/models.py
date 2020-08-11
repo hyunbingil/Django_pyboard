af4f60db0eaa7d4c3635adbc2126c3725fac761f
hyunbingil <super199g@naver.com>
2020-08-11 14:48:35 +0900

    ✨ 추천하기 기능 추가 / 🔧 앵커 추가

--- mysite/pybo/models.py
+++ mysite/pybo/models.py
@@ -5,9 +5,10 @@ class Question(models.Model):
     subject = models.CharField(max_length=200) # 제목 : 최대 200자 / 글자 수 제한 O(CharField)
     content = models.TextField() # 내용 : 글자 수 제한 X(TextField)
     create_date = models.DateTimeField() # 작성 일시 : 날짜와 시간에 관계된 속성(DateTimeField)
-    author = models.ForeignKey(User, on_delete=models.CASCADE) # 저자
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author_question") # 저자
     modify_date = models.DateTimeField(null=True, blank=True) # 수정 일시
     # django.contrib.auth.models 패키지의 User이므로 글쓴이에 해당되는 author 속성은 User모델을 ForeignKey로 적용해야 한다.
+    voter = models.ManyToManyField(User, related_name="voter_question") # voter 추가, 다대다 관계를 의미하는 ManyToManyField를 사용한다.
     def __str__(self):
         return self.subject
 
@@ -18,8 +19,9 @@ class Answer(models.Model):
     # 이 답변과 연결된 질문(Question)이 삭제될 경우 답변(Answer)도 함께 삭제된다는 의미.
     content = models.TextField()
     create_date = models.DateTimeField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author_answer")
     modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name="voter_answer")
     def __str__(self):
         return self.subject
 
