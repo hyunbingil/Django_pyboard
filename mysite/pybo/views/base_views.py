15f55b98934840d51013b8972f28a4b929902122
hyunbingil <super199g@naver.com>
2020-08-14 10:41:05 +0900

    ✨ 검색과 정렬 기능 추가

--- mysite/pybo/views/base_views.py
+++ mysite/pybo/views/base_views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-
+from django.db.models import Q, Count
 from ..models import Question
 
 def index(request):
@@ -8,20 +8,32 @@ def index(request):
     pybo 목록 출력
     """
     # 입력 파라미터
-    page = request.GET.get('page', '1') # 페이지
-    # page값이 없을 경우에는 디폴트로 1이라는 값을 설정
-    
-    # 조회
-    question_list = Question.objects.order_by('-create_date')
-    # 질문 목록 데이터를 얻는다.
-    # order_by는 조회 결과를 정렬하는 함수.
-    # create_date를 작성 일시의 역순으로(-) 정렬하라.
+    page = request.GET.get('page', '1')  # 페이지
+    kw = request.GET.get('kw', '')  # 검색어
+    so = request.GET.get('so', 'recent')  # 정렬기준
+
+    # 정렬
+    if so == 'recommend':
+        question_list = Question.objects.annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    elif so == 'popular':
+        question_list = Question.objects.annotate(num_answer=Count('answer')).order_by('-num_answer', '-create_date')
+    else:  # recent
+        question_list = Question.objects.order_by('-create_date')
+
+    # 검색
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목검색
+            Q(content__icontains=kw) |  # 내용검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이검색
+            Q(answer__author__username__icontains=kw)  # 답변 글쓴이검색
+        ).distinct()
 
     # 페이징 처리
     paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw, 'so': so}
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
